4b5e5fa017d7e6c77faeae91c70a9b933e455922
Manoj <manoj.u2907@gmail.com>
2024-11-25 11:48:06 +0530

    Added Test Method2

--- src/test/java/tests/TestExamples.java
+++ src/test/java/tests/TestExamples.java
@@ -6,6 +6,9 @@ import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;
 import io.restassured.response.Response;
 
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
 public class TestExamples {
 	
 	@Test
@@ -26,10 +29,13 @@ public class TestExamples {
 	public void test_2() {
 		
 		baseURI="https://reqres.in/api";
+		
 		given().
 			get("users?page=2").
 		then().
-			statusCode(200);
+			statusCode(200).
+			body("data[1].id", equalTo(8)).
+			log().all();
 		
 		
 	}
